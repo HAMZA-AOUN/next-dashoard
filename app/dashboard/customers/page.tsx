5aebf497840652671dd9bb5de6a97db6a2801827
Hamza Aoun <aou4925@gmail.com>
2024-05-02 23:36:56 +0300

    Project completed

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Metadata } from 'next';
 
-const CustomersPage = () => {
+export const metadata: Metadata = {
+  title: 'Customer',
+};
+
+const CustomersPage = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
   return <div>CustomersPage</div>;
 };
 
